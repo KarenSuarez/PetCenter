2ed1262bcf3f2876c17c25f3006a40d987df40a1
WilliamC111 <110690003+WilliamC111@users.noreply.github.com>
2023-10-26 10:30:59 -0500

    Librerias faltantes

--- src/main/java/co/edu/uptc/controller/PetController.java
+++ src/main/java/co/edu/uptc/controller/PetController.java
@@ -1,10 +1,16 @@
 package co.edu.uptc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import co.edu.uptc.model.Pet;
 import co.edu.uptc.repository.PetRepository;
+import java.util.Optional;
 
 import java.util.Optional;
 
